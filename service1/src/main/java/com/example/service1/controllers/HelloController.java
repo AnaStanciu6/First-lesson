aae1a828d10b9f4271bf15349b8c8b83d44d3602
Ana Maria Stanciu <Ana.Stanciu@endava.com>
2019-10-24 17:09:31 +0300

    First commit

---
+++ service1/src/main/java/com/example/service1/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.service1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @PostMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello," + name;
+    }
+
+}
